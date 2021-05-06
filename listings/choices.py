a42c5bfd4ccfd10c6d6ffa38287eb3de4634aaa3
unknown <rahullaniya1210@gmail.com>
2021-05-06 20:52:14 +0530

    Initial commit

---
+++ listings/choices.py
@@ -0,0 +1,62 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+
+}
+
+price_choices = {
+    '10000000': '₹10,000,000',
+    '20000000': '₹20,000,000',
+    '30000000': '₹30,000,000',
+    '40000000': '₹40,000,000',
+    '50000000': '₹50,000,000',
+    '60000000': '₹60,000,000',
+    '70000000': '₹70,000,000',
+    '80000000': '₹80,000,000',
+    '90000000': '₹90,000,000',
+    '100000000': '₹100,000,000'
+}
+
+state_choices = {
+    'AK': 'Andhra Pradesh',
+    'AL': 'Andaman and Nicobar Islands',
+    'AR': 'Arunachal Pradesh',
+    'AS': 'Assam',
+    'AZ': 'Bihar',
+    'CA': 'Chhattisgarh',
+    'CO': 'Delhi',
+    'CT': 'Goa',
+    'DC': 'Gujarat',
+    'DE': 'Haryana',
+    'FL': 'Himachal Pradesh',
+    'GA': 'Jammu and Kashmir',
+    'GU': 'Jharkhand',
+    'HI': 'Karnataka',
+    'IA': 'Kerala',
+    'ID': 'Madhya Pradesh',
+    'IL': 'Maharashtra',
+    'IN': 'Manipur',
+    'KS': 'Meghalaya',
+    'KY': 'Mizoram',
+    'LA': 'Nagaland',
+    'MA': 'Odisha',
+    'MD': 'Punjab',
+    'ME': 'Rajasthan',
+    'MI': 'Sikkim',
+    'MN': 'Tamil Nadu',
+    'MO': 'Telangana',
+    'MP': 'Tripura',
+    'MS': 'Uttar Pradesh',
+    'MT': 'Uttarakhand',
+    'NA': 'West Bengal'
+    
+
+}
